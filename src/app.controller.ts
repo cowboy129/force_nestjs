b3ad2d3e8baae4c1bb2e02d020d13b00ac4b5550
Xingrun CHEN <chenxingrun@bytedance.com>
2025-11-07 11:13:50 +0800

    add sp

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('sp')
+  getStaticPage(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  }
 }
